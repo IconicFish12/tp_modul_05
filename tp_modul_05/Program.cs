0efb4b19058a5dbe5621049cc84889225931d151
Ibnu Syawal Aliefian <superglidingogre0571@gmail.com>
2025-03-16 10:57:14 +0700

    add HalloGeneric class with method sapaUser

--- tp_modul_05/Program.cs
+++ tp_modul_05/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace tp_modul_05
+{
+    public class HalloGeneric
+    {
+
+        public void sapaUser<T>(T entity)
+        {
+            Console.WriteLine($"Halo Nama Saya {entity}");
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string nama;
+
+            HalloGeneric user = new HalloGeneric();
+
+            Console.Write("Masukan Nama kamu : ");
+            nama = Console.ReadLine();
+
+            user.sapaUser(nama);
+
+        }
+    }
+}
